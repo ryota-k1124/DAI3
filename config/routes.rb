8ec54e6c858fd89fc12869f948f59c3011149dda
ryota <ryota-k@nttpc.co.jp>
2018-01-07 08:59:40 +0000

    admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   #get 'instagrams' => 'instagrams#index'
   resources :instagrams, only: [:index, :new, :create, :edit, :update, :destroy, :show]
   #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
