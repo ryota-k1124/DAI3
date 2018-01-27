b68a31323e187a99a408b90541623bff01ff63da
ryota <ryota-k@nttpc.co.jp>
2018-01-27 06:05:49 +0000

    img

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
-  return image_tag(user.avatar, alt: user.name) if user.avatar?
   def profile_img(user)
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
     unless user.provider.blank?
       img_url = user.image_url
     else
