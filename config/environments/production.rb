64f0466b33d9251c7278d2ef1262313247a22e2c
ryota <ryota-k@nttpc.co.jp>
2018-01-28 15:53:54 +0000

    mod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'evening-caverns-37566.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '54.238.110.131' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
