405d8e01ceba78ee4de0c698f3368fa6e64af217
ryota <ryota-k@nttpc.co.jp>
2018-02-04 00:27:32 +0000

    mod color

--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -1,5 +1,5 @@
 class Instagram < ActiveRecord::Base
-    validates :image, presence: true
+    validates_presence_of :image, :title
     belongs_to :user
     has_many :comments, dependent: :destroy
     mount_uploader :image, PictureUploader
