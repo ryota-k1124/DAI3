f056c869340a4cdfeed65621703762399916e4c7
ryota <ryota-k@nttpc.co.jp>
2018-02-04 00:24:23 +0000

    mod color

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :content, presence: true
   belongs_to :user
   belongs_to :instagram
   has_many :notifications, dependent: :destroy
--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -1,4 +1,5 @@
 class Instagram < ActiveRecord::Base
+    validates :image, presence: true
     belongs_to :user
     has_many :comments, dependent: :destroy
     mount_uploader :image, PictureUploader
