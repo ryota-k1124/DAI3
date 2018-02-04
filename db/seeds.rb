7922318c73ba0b9c9cd4dca8fb30b5941aeb8790
ryota <ryota-k@nttpc.co.jp>
2018-02-04 00:44:27 +0000

    mod color

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+n = 1
+while n <= 10
+
+  sn = #{n}.to_s
+  email = Faker::Internet.email
+  password = "password"
+  name = "test#{sn}"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+
+  Instagram.create(
+    content: "hoge",
+    image: File.open("./app/assets/images/rails.png"),
+    user_id: n
+  )
+  n = n + 1
+
+end
